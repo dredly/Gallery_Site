97a5aa47c693be3ce7965ced4176f0912a153b40
dredly <dredly@gmail.com>
2022-01-27 21:49:17 +0000

    Basic express setup

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const ejsMate = require('ejs-mate');
+
+const app = express();
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.get('/admin/upload', (req, res) => {
+    res.send('FORM TO UPLOAD IMAGE');
+})
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+})
