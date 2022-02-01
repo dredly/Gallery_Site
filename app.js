4d0122abe111b43bebbf37adc39ad636851aba62
dredly <dredly@gmail.com>
2022-02-01 00:01:16 +0000

    Started work on CRUD for art pieces

--- app.js
+++ app.js
@@ -19,16 +19,38 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/static'));
 app.use(morgan('tiny'));
 
-app.get('/', async (req, res) => {
-    const artpieces = await Artpiece.find({});
-    res.render('index', { artpieces });
+app.get('/', (req, res) => {
+    res.redirect('/gallery');
 })
 
-app.get('/admin/upload', (req, res) => {
-    res.send('FORM TO UPLOAD IMAGE');
+app.get('/gallery', async (req, res) => {
+    const artpieces = await Artpiece.find({});
+    res.render('artpieces/index', { artpieces });
+})
+
+app.get('/gallery/new', (req, res) => {
+    res.render('artpieces/new');
+})
+
+app.get('/gallery/:id', async (req, res) => {
+    const artpiece = await Artpiece.findById(req.params.id);
+    res.render('artpieces/show', { artpiece });
+})
+
+app.post('/gallery', async (req, res) => {
+    const { title, filename, description } = req.body;
+    const tags = req.body.tags.split(',');
+    const artpiece = new Artpiece({ title, filename, description, tags });
+    await artpiece.save();
+    res.redirect(`/gallery/${artpiece._id}`);
+})
+
+app.delete('/gallery/:id', async (req, res) => {
+    res.send("SO CALLED DELETING");
 })
 
 app.listen(3000, '0.0.0.0', () => {
