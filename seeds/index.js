20a0ba0642ce6d84e2e48bde2ceabc3cda136bb4
dredly <dredly@gmail.com>
2022-02-14 15:51:37 +0000

    Seeded images are now moved to the proper storage location

--- seeds/index.js
+++ seeds/index.js
@@ -1,18 +1,44 @@
 const mongoose = require('mongoose');
 const fs = require('fs');
+const path = require('path');
 const process = require("process")
 const Artpiece = require('../models/artpiece');
 
 mongoose.connect('mongodb://localhost:27017/gallery');
 
-const img_dir = "./seeds/seed_imgs";
+const seed_img_dir = "./seeds/seed_imgs";
+const img_dir = "./static/img";
 const filenames = [];
-fs.readdir(img_dir, (err, files) => {
+fs.readdir(seed_img_dir, (err, files) => {
     if (err) {
         console.error("Could not list the directory.", err);
         process.exit(1);
     }
     files.forEach(file => {
+        // Code adapted from static overflow answer by Mikey A. Leonetti
+        // https://stackoverflow.com/questions/32511789/looping-through-files-in-a-folder-node-js
+        // https://stackoverflow.com/users/3282410/mikey-a-leonetti
+        const fromPath = path.join(seed_img_dir, file);
+        const toPath = path.join(img_dir, file);
+        fs.stat(fromPath, function (error, stat) {
+            if (error) {
+                console.error("Error stating file.", error);
+                return;
+            }
+
+            if (stat.isFile())
+                console.log("'%s' is a file.", fromPath);
+            else if (stat.isDirectory())
+                console.log("'%s' is a directory.", fromPath);
+
+            fs.rename(fromPath, toPath, function (error) {
+                if (error) {
+                    console.error("File moving error.", error);
+                } else {
+                    console.log("Moved file '%s' to '%s'.", fromPath, toPath);
+                }
+            });
+        });
         filenames.push(file);
     })
 })
