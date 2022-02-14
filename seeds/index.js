801630da4bd102daacbe40c22da3f3f70c429e53
dredly <dredly@gmail.com>
2022-02-14 15:31:22 +0000

    Setup seeds script to loop through provided seed images

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,21 @@
 const mongoose = require('mongoose');
+const fs = require('fs');
+const process = require("process")
 const Artpiece = require('../models/artpiece');
 
 mongoose.connect('mongodb://localhost:27017/gallery');
+
+const img_dir = "./seeds/seed_imgs";
+const filenames = [];
+fs.readdir(img_dir, (err, files) => {
+    if (err) {
+        console.error("Could not list the directory.", err);
+        process.exit(1);
+    }
+    files.forEach(file => {
+        filenames.push(file);
+    })
+})
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, "connection error"));
@@ -11,10 +25,10 @@ db.once('open', () => {
 
 const seedDB = async () => {
     await Artpiece.deleteMany({});
-    for (let i = 0; i < 30; i++) {
+    for (let i = 0; i < filenames.length; i++) {
         const artpiece = new Artpiece({
             title: `Art version ${i}`,
-            filename: `img${i}.jpg`,
+            filename: filenames[i],
             description: `This is an amazing piece of art`,
             tags: ['mixed media', 'original']
         })
