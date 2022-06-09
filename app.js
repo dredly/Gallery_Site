07b0d35d2d7d101200422223dd5fd57e45033af3
Daniel Redly <RedlyDP@cardiff.ac.uk>
2022-06-09 17:51:21 +0200

    preparing for test deployment

--- app.js
+++ app.js
@@ -69,6 +69,7 @@ app.use('/gallery', galleryRoutes)
 
 app.use('/admin', adminRoutes)
 
-app.listen(3000, '0.0.0.0', () => {
-	console.log('Listening on port 3000')
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`)
 })
